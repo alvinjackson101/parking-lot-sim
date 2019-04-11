0124add60d51ed6698bb74814f02513c3aee4beb
Alvin Jackson <alvinjackson@Alvins-MBP.attlocal.net>
2019-04-10 21:40:46 -0500

    updated js parking 10 cars

--- index.js
+++ index.js
@@ -1,56 +1,77 @@
-
 const maxParkedTime = 5000;
-const CarFactoryLimit = 12;
+const CarFactoryLimit = 100;
 
-const Dates = [ "2019" , "2000" , "2017" , "2005" , "1995" , "1980"];
-const carPlates = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "1", "2", "3", "4", "5", "6"];
-const carModels = [ "Porsche" , "Lamborghini" , "Ferrari" , "Tesla" , "BMW" ];
+const Dates = [ "2019" , "2000" , "2017" , "2005" , "1995" , "1980"]; //car random dates
+const carPlates = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "1", "2", "3", "4", "5", "6"]; //car random plates
+const carModels = [ "Porsche" , "Lamborghini" , "Ferrari" , "Tesla" , "BMW" ]; //random car models
 
 const MaxParkingSpaces = 10;
 const parkingLot = [];
 
-function Car(){
+const carsWaiting = [];
+const carsLeft = [];
 
+function Car(){ 
     this.plates = carPlates[Math.floor(Math.random() * carPlates.length)];
     this.make = carModels[Math.floor(Math.random() * carModels.length)];
     this.year = Dates[Math.floor(Math.random() * Dates.length)];
     this.time = Math.floor(Math.random() * maxParkedTime);
     this.parked = false;
 
-    this.park = function(){
+    this.park = function(car){ 
+        console.log("Car is Parking", car)
+        let parkedCar = parkingLot.push(car);
         this.parked = true;
-        setTimeout(function(e){
-            e.leave();
-        
-        },
-        this.time,
-        this);
-
-        console.log("parked!");
-        console.dir(this);
-
-        this.leave = function() {
-            console.log("leaving...");
-            this.parked = false;
-            console.log("left!");
-            console.dir(this);
-        }
+        setTimeout(function(){car.leave(parkedCar)}, this.time); //timer of car parked
     }
 
-    return this;
+    this.leave = function(car) {
+        console.log("Car is Leaving!", car)
+        this.parked = false;
+        carsLeft.push(car);
+        
+        parkCar();
+    }
 }
+
+console.log("leaving(car)");
 
 function carFactory() {
-    let newCars = [];
-
     for(let i = 0; i < CarFactoryLimit; i++) {
-       newCars.push(new Car())
+       carsWaiting.push(new Car())
     }
 
-    return newCars;
+    return carsWaiting;
 }
 
+
+
+// function createSpaces(){
+//     this.taken = false;
+// }
+
+//function attendant(){
+   // if(parkingLot.length < MaxParkingSpaces){
+    //    this.taken = true;
+//        console.log(car.push
+
+   // }
+
+
+function parkCar() {
+    console.log(carsWaiting)
+    if( parkingLot.length <= MaxParkingSpaces){
+        for(var i = 0; i < MaxParkingSpaces; i++){
+            let car = carsWaiting.pop();
+            car.park(car);
+        }
+    } else {
+        console.log("Lot is full, please wait.")
+    }
+}
+
+console.log(parkingLot);
 
 const cars = carFactory();
 
-console.log(cars)
+parkCar();
